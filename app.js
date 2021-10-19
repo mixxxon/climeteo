5f4f78680690e9a98df4c51dbbe85377006d003b
bexony <bexony@gmail.com>
2021-10-19 17:42:58 +0200

    Please wait message added

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const request = require("postman-request");
 const dateFormate = require("./dateFormate.js");
 
+console.log("Connecting to a service. Please wait...");
+
 const url = "https://www.7timer.info/bin/civillight.php?lon=20.5&lat=44.8&ac=0&unit=metric&output=json&tzshift=0";
 
 request(url, (err, res, body) => {
 
     if(err){
-
+        console.clear();
         console.log("There seems to be a problem with the weather service. Here's the details: ");
         console.log(err.message);
 
     } else {
+
+        console.clear();
 
         const data = JSON.parse(body);
     
