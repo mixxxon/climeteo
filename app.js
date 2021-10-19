dbece0d1ef4f973b464f5e04bc7ffac50f9660ab
bexony <bexony@gmail.com>
2021-10-19 15:20:30 +0200

    Error handling added

--- app.js
+++ app.js
@@ -5,16 +5,24 @@ const url = "https://www.7timer.info/bin/civillight.php?lon=20.5&lat=44.8&ac=0&u
 
 request(url, (err, res, body) => {
 
-    const data = JSON.parse(body);
+    if(err){
+
+        console.log("There seems to be a problem with the weather service. Here's the details: ");
+        console.log(err.message);
+
+    } else {
+
+        const data = JSON.parse(body);
     
-    const date = dateFormate(data.dataseries[0].date);
-    const dailyHigh = data.dataseries[0].temp2m.max;
-    const dailyMin = data.dataseries[0].temp2m.min;
-    const weather = data.dataseries[0].weather;
+        const date = dateFormate(data.dataseries[0].date);
+        const dailyHigh = data.dataseries[0].temp2m.max;
+        const dailyMin = data.dataseries[0].temp2m.min;
+        const weather = data.dataseries[0].weather;
+    
+        console.log("This is weather forecast for " + date + "\n");
+        console.log("Weather: " + weather);
+        console.log("Highest temp. today: " + dailyHigh + "\u00B0C");
+        console.log("Lowest temp. today: " + dailyMin + "\u00B0C");
 
-    console.log("This is weather forecast for " + date + "\n");
-    console.log("Weather: " + weather);
-    console.log("Highest temp. today: " + dailyHigh + "\u00B0C");
-    console.log("Lowest temp. today: " + dailyMin + "\u00B0C");
-
+    }
 });
